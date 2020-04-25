7062b6a94aff4ed229e7021bf2feb4b1e605ce43
yangjian <yangjian@ishansong.com>
2020-04-25 10:06:01 +0800

    格式化输出

--- port-scan/port-scan.sh
+++ port-scan/port-scan.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+function current_time_str() {
+	echo $(date "+%Y-%m-%d %H:%M:%S")
+}
+
+function format_msg() {
+	echo "$(current_time_str): $1"
+}
 function scan_port() {
-	echo "nc -v -w 1 $1 -z $2 2>&1|grep succeed >> scan-result.txt" >> scan.log
+	echo $(format_msg "nc -v -w 1 $1 -z $2 2>&1|grep succeed >> scan-result.txt") >> scan.log
 	nc -v -w 1 $1 -z $2 2>&1|grep succeed >> scan-result.txt && {
-		echo "scan success, ip: $1, range: $2" >> scan-success.log
+		echo $(format_msg "scan success, ip: $1, range: $2") >> scan-success.log
 	} || {
-		echo "scan fail, ip: $1, range: $2" >> scan-fail.log
+		echo $(format_msg "scan fail, ip: $1, range: $2") >> scan-fail.log
 	}
 }
 
 ip_addr=39.108.105.130
-batch=100
+batch=3
 begin=$1
 end=$2
 
@@ -19,7 +26,7 @@ mkfifo $tmp_fifofile      # 新建一个fifo类型的文件
 exec 6<>$tmp_fifofile      # 将fd6指向fifo类型
 rm $tmp_fifofile
 
-thread=10 # 此处定义线程数
+thread=5 # 此处定义线程数
 for ((i=0;i<$thread;i++));do 
 echo
 done >&6 # 事实上就是在fd6中放置了$thread个回车符
@@ -47,6 +54,4 @@ if [ $remain -gt 0 ]; then
 	} &
 fi
 
-echo "扫描完成[$1-$2]"
-
-
+echo $(format_msg "扫描完成[$1-$2]")
